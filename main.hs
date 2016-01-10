316cf4fc22db56d7e10d70a674b2312f7e3de0d1
Aaron Chan <aaronchan@Aarons-MacBook-Pro-8.local>
2016-01-10 10:01:05 -0700

    fixd delta calcs, added grad descent

--- main.hs
+++ main.hs
@@ -26,17 +26,26 @@ forward xs ws = last $ feedForward xs ws []
 getOutputDeltas :: [Double] -> [Double] -> [Double]
 getOutputDeltas actuals outs = zipWith (\y out -> -(y - out) * out * (1 - out)) actuals outs
 
-getDelta :: Double -> [Double] -> Double -> Double
-getDelta a wvec d = sum(map (\w -> w * d) wvec) * a * (1 - a)
+getDelta :: Double -> [Double] -> [Double] -> Double
+getDelta a wvec dvec = trace (s) sum(zipWith (*) wvec dvec) * a * (1 - a)
+                        where s = "Combining a: " ++ show a ++ " and d: " ++ show dvec ++ " with w: " ++ show wvec ++ ".  LEN(D): " ++ show(length dvec) ++ " vs LEN(W): " ++ show(length wvec)
 
 getDeltaVec :: [Double] -> [[Double]] -> [Double] -> [Double] -> [Double]
-getDeltaVec (a:as) (w:ws) (d:[]) accum = trace ("already here") (accum ++ [getDelta a w d])
-getDeltaVec (a:as) (w:ws) (d:ds) accum = getDeltaVec as ws ds (accum ++ trace ("delta: " ++ show (getDelta a w d) ++ " a, w, d: " ++ show a ++ "," ++ show w ++ "," ++ show d) ([getDelta a w d]))
+getDeltaVec [] ws ds accum = trace ("DELTA VECTOR: " ++ show accum) accum
+getDeltaVec (a:as) (w:ws) ds accum = getDeltaVec as ws ds (accum ++ trace (debug_m) ([thisDelta]))
+                                        where thisDelta = getDelta a w ds
+                                              debug_m = "Result delta: " ++ show thisDelta
 
 getDeltas :: [[Double]] -> [[[Double]]] -> [Double] -> [[Double]] -> [[Double]]
-getDeltas [] [] ds accum = accum
-getDeltas (a:as) (w:ws) ds accum = trace ("ds': " ++ show ds') (getDeltas as ws ds' (accum ++ [ds']))
+getDeltas [] ws ds accum = accum
+getDeltas (a:as) (w:ws) ds accum = getDeltas as ws ds' (accum ++ [ds'])
                                     where ds' = getDeltaVec a (transpose w) ds []
+
+gradientDescent :: [[Double]] -> [[Double]] -> [[[Double]]] -> [[[Double]]]
+gradientDescent as [] accum = accum
+gradientDescent (a:as) (d:ds) accum = gradientDescent as ds $ accum ++ [[[-alpha * x * y | x <- a'] | y <- d]]
+                                        where a' = a ++ [1] -- Account for bias unit.
+                                              alpha = 2
 
 mat2zip :: (Double -> Double -> Double) -> [[Double]] -> [[Double]] -> [[Double]]
 mat2zip f a b = zipWith (\c d -> zipWith f c d) a b
@@ -44,16 +53,21 @@ mat2zip f a b = zipWith (\c d -> zipWith f c d) a b
 mat3zip :: (Double -> Double -> Double) -> [[[Double]]] -> [[[Double]]] -> [[[Double]]]
 mat3zip f a b = zipWith (mat2zip f) a b
 
-backPropagation :: [Double] -> [[Double]] -> [[[Double]]] -> [[Double]]
-backPropagation actuals as ws = changes
-                                    where changes = trace ("ds " ++ show ds ++ " and as: " ++ show as) (mat2zip (*) ds as)
-                                          ds = trace ("STARTING WITH AS, WS, OUTS: " ++ show as ++ "," ++ show ws ++ "," ++ show outputDeltas) (getDeltas as ws outputDeltas [])
+backPropagation :: [Double] -> [Double] -> [[Double]] -> [[[Double]]] -> [[[Double]]]
+backPropagation xs actuals as ws = trace ("Changes: " ++ show changes) (mat3zip (+) changes ws)
+                                    where changes = gradientDescent (xs:as) ds []
+                                          ds = reverse $ outputDeltas : trace (debug) (getDeltas (tail (reverse as)) (reverse ws) outputDeltas [])
                                           outputDeltas = getOutputDeltas actuals (last as)
+                                          debug = "INITIAL AS, WS, OUTS: " ++ show as ++ "," ++ show ws ++ "," ++ show outputDeltas
+
+
+
+
 weights :: [[[Double]]]
 weights = [[[2.0, -3.0], [-4, -5]], [[1, -2, 3]]]
 
 test_as :: [[Double]]
-test_as = [[1, 0], [1]]
+test_as = [[0.6, 0.2], [0.9998]]
 
 desired :: [Double]
 desired = [0]
